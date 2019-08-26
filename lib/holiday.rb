8653248d90f921de6627c8ebef47de58d6ad61de
Sara McCombs <thesaramccombs@gmail.com>
2019-08-26 00:48:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|word| word.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
+  end
 
 end
 
