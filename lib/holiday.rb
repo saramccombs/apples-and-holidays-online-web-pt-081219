1e24f7d4e09a7d024c3dfcbdf41fd0da41d1e235
Sara McCombs <thesaramccombs@gmail.com>
2019-08-26 01:18:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,6 +91,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.collect do |holiday, supplies|
       if supplies.include?("BBQ")
         array << holiday
+      end
     end
   end
   array.flatten.compact
