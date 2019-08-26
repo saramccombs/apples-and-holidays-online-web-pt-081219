ce2529df69126598a11bc7df1d7b8aedb17c592a
Sara McCombs <thesaramccombs@gmail.com>
2019-08-26 01:10:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.collect do |season, holiday_with_supplies|
     season.collect do |holiday_name, supplies|
-      if holiday_name.includes?("BBQ") 
+      if supplies.includes?("BBQ") 
         array_holiday_names << holiday_name
       end
     end
